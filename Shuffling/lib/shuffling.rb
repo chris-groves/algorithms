fc7042b69e296c3693624924cca43d11892aea1f
Chris Groves <chris_groves87@hotmail.co.uk>
2019-12-19 11:38:42 +0000

    Finish writing own algorithm for shuffle

--- Shuffling/lib/shuffling.rb
+++ Shuffling/lib/shuffling.rb
@@ -1,3 +1,9 @@
 def shuffling(array)
-  array
+  if array.length == 0 || array.length == 1
+    array
+  else
+    output_array = []
+    array.each { |number| output_array.insert(rand(0..output_array.length), number) }
+    output_array
+  end
 end
--- Shuffling/spec/shuffling_spec.rb
+++ Shuffling/spec/shuffling_spec.rb
@@ -1,7 +1,23 @@
 require 'shuffling'
 
 describe 'shuffling' do
-it 'returns an empty array for input of an empty array' do
-  expect(shuffling([])).to eq []
-end
+  it 'returns an empty array for input of an empty array' do
+    expect(shuffling([])).to eq []
+  end
+
+  it 'returns the array as it is for input of an array of length 1' do
+    expect(shuffling([1])).to eq [1]
+  end
+
+  it 'swaps index positions of numbers in an array of length 2' do
+    expect(shuffling([1,2])).to eq [2,1]
+  end
+
+  it 'output of an array of length 3 is different to input array' do
+    expect(shuffling([1,2,3])).to_not eq [1,2,3]
+  end
+
+  it 'output of an array of length 10 is different to input array' do
+    expect(shuffling([1,2,3,4,5,6,7,8,9,10])).to_not eq [1,2,3,4,5,6,7,8,9,10]
+  end
 end
