eb867c834c2afc94587cf43c135bdbff624c81aa
Chris Groves <chris_groves87@hotmail.co.uk>
2019-12-20 18:47:12 +0000

    Refactor benchmarking of orginal shuffle method -output now saves to a file (output.txt) ready to be analysed

--- shuffle.rb
+++ shuffle.rb
@@ -1,16 +1,25 @@
 require 'benchmark'
 
+file = File.open('output.txt', 'w')
+
 x = 50000
 
 array = Array(1..x)
 
 while true do
-  Benchmark.bm do |bm|
-    bm.report("Test Array #{x}:") do
-      array.shuffle
+  counter = 1
+  while true do
+    time = Benchmark.realtime do
+        array.shuffle
     end
+
+    file.puts "#{x} - #{time*1000}"
+    counter += 1
+    break if counter == 100
   end
   x += 50000
   array = Array(1..x)
   break if x == 1050000
 end
+
+file.close
