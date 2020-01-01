039093992c8fb74a4c49ebed747e40e0157a98d1
Chris Groves <chris_groves87@hotmail.co.uk>
2020-01-01 16:07:04 +0000

    Repeat code for sort, reverse and last methods

--- lib/last.rb
+++ lib/last.rb
@@ -1,26 +1,80 @@
 require 'benchmark'
 
 class Last
-  attr_reader :array_length, :times_array
+  attr_accessor :array_length, :array, :results_array, :file, :average_time
+
+  INCREASE_ARRAY_LENGTH_AMOUNT = 50000
 
   def initialize(array_length)
     @array_length = array_length
-    @times_array = []
+    @results_array = []
+    @file = ""
+    @array = []
+    @average_time = 0
+  end
+
+  def open_output_file
+    @file = File.open('last_output.txt', 'a')
+  end
+
+  def close_output_file
+    @file.close
   end
 
   def build_array
-    @array = Array(1..array_length)
+    @array = Array(1..@array_length)
   end
 
-  def timing
-    starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    @array.last
-    ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    time = ending - starting
-    p time
+  def time_shuffle_method
+    Benchmark.realtime { @array.last }
+  end
+
+  def add_time_to_results_array
+    @results_array << time_shuffle_method
+  end
+
+  def sort_results_array
+    @results_array = @results_array.sort
+  end
+
+  def remove_largest_5_results
+    @results_array.pop(5)
+  end
+
+  def remove_smallest_5_results
+    @results_array.shift(5)
+  end
+
+  def calculate_average_time
+    @average_time = @results_array.sum/@results_array.length
+  end
+
+  def output_results_to_file
+    @file.puts "#{@array_length} - #{@average_time}"
+  end
+
+  def empty_results_array
+    @results_array = @results_array.clear
+  end
+
+  def increase_array_length
+    @array_length += INCREASE_ARRAY_LENGTH_AMOUNT
   end
 end
 
-timer = Last.new(5)
-timer.build_array
-timer.timing
+timer = Last.new(50000)
+
+while true do
+  timer.build_array
+  100.times { timer.add_time_to_results_array }
+  timer.sort_results_array
+  timer.remove_largest_5_results
+  timer.remove_smallest_5_results
+  timer.calculate_average_time
+  timer.open_output_file
+  timer.output_results_to_file
+  timer.close_output_file
+  timer.empty_results_array
+  timer.increase_array_length
+  break if timer.array_length == 1050000
+end
--- lib/sort.rb
+++ lib/sort.rb
@@ -1,17 +1,85 @@
 require 'benchmark'
 
-x = 50000
+class Sort
+  attr_accessor :array_length, :array, :results_array, :file, :average_time
 
-array = Array(1..x)
+  INCREASE_ARRAY_LENGTH_AMOUNT = 50000
+
+  def initialize(array_length)
+    @array_length = array_length
+    @results_array = []
+    @file = ""
+    @array = []
+    @average_time = 0
+  end
+
+  def open_output_file
+    @file = File.open('sort_output.txt', 'a')
+  end
+
+  def close_output_file
+    @file.close
+  end
+
+  def build_array
+    @array = Array(1..@array_length)
+  end
+
+  def shuffle_array
+    @array = @array.shuffle
+  end
+
+  def time_shuffle_method
+    Benchmark.realtime { @array.sort}
+  end
+
+  def add_time_to_results_array
+    @results_array << time_shuffle_method
+  end
+
+  def sort_results_array
+    @results_array = @results_array.sort
+  end
+
+  def remove_largest_5_results
+    @results_array.pop(5)
+  end
+
+  def remove_smallest_5_results
+    @results_array.shift(5)
+  end
+
+  def calculate_average_time
+    @average_time = @results_array.sum/@results_array.length
+  end
+
+  def output_results_to_file
+    @file.puts "#{@array_length} - #{@average_time}"
+  end
+
+  def empty_results_array
+    @results_array = @results_array.clear
+  end
+
+  def increase_array_length
+    @array_length += INCREASE_ARRAY_LENGTH_AMOUNT
+  end
+end
+
+timer = Sort.new(50000)
 
 while true do
-  Benchmark.bm do |bm|
-    bm.report("Test Array #{x}:") do
-      array.sort
-    end
-  end
-  x += 50000
-  array = Array(1..x)
-  array.shuffle
-  break if x == 1050000
+  timer.build_array
+  timer.shuffle_array
+  100.times { timer.add_time_to_results_array }
+  timer.sort_results_array
+  timer.remove_largest_5_results
+  timer.remove_smallest_5_results
+  timer.calculate_average_time
+  timer.open_output_file
+  timer.output_results_to_file
+  timer.close_output_file
+  timer.empty_results_array
+  timer.increase_array_length
+  break if timer.array_length == 1050000
 end
