8d2ff8cb34ad70cf77bfff297f024afc391aa086
Chris Groves <chris_groves87@hotmail.co.uk>
2020-01-02 16:13:41 +0000

    Refactor most frequent words algorithm -wrap into class and methods

--- Most-Frequent-Words/lib/most_frequent_words.rb
+++ Most-Frequent-Words/lib/most_frequent_words.rb
@@ -1,28 +1,54 @@
-def most_frequent_words(array)
-  output_array = []
+class Words
+  attr_accessor :array, :output_array, :hash
 
-  array.each do |word|
-    if output_array.include?(word) == false
-      output_array << word
+  def initialize(array)
+    @array = array
+    @output_array = []
+    @hash = {}
+    @answer_array = []
+  end
+
+  def identify_unique_words
+    @array.each do |word|
+      if output_array.include?(word) == false
+        @output_array << word
+      end
     end
   end
 
-  hash = output_array.product([0]).to_h
-
-  array.map do |word|
-    hash[word] += 1
+  def create_hash_of_unique_words
+      @hash = output_array.product([0]).to_h
   end
 
-  answer_array = []
+  def store_count_of_each_unique_word_in_hash
+    @array.map do |word|
+      @hash[word] += 1
+    end
+  end
 
-  hash.each {|key,value| answer_array << key if value == hash.values.max }
+  def add_most_frequent_word_to_answer_array
+    @hash.each {|key,value| @answer_array << key if value == @hash.values.max }
+  end
 
-  hash = hash.each {|key,value| hash.delete(key) if value == hash.values.max }
+  def remove_most_frequent_word_from_hash
+    @hash = @hash.each {|key,value| @hash.delete(key) if value == @hash.values.max }
+  end
 
-  if hash.length == 0
-    answer_array
-  else
-    hash.each {|key,value| answer_array << key if value == hash.values.max }
-    answer_array
+  def return_answer
+    if @hash.length == 0
+      @answer_array
+    else
+      @hash.each {|key,value| @answer_array << key if value == @hash.values.max }
+      @answer_array
+    end
+  end
+
+  def most_frequent_words(array)
+    identify_unique_words
+    create_hash_of_unique_words
+    store_count_of_each_unique_word_in_hash
+    add_most_frequent_word_to_answer_array
+    remove_most_frequent_word_from_hash
+    return_answer
   end
 end
