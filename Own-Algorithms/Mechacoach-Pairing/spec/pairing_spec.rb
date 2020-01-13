91205e69a8b59ddc6045fd8ac2b7e9eb8f256f12
Chris Groves <chris_groves87@hotmail.co.uk>
2020-01-13 19:06:11 +0000

    Tidy up files for pairing algorithm

--- Own-Algorithms/Mechacoach-Pairing/spec/pairing_spec.rb
+++ Own-Algorithms/Mechacoach-Pairing/spec/pairing_spec.rb
@@ -1,12 +1,6 @@
 require 'pairing'
 
 describe Pairing do
-
-  # it 'returns the input array for input of an array with two element' do
-  #   pairing = Pairing.new(["Alice","Bob"])
-  #   expect(pairing.pairing).to eq [["Alice","Bob"]]
-  # end
-
   it 'returns 3 combinationa for an input array with 4 elements' do
     pairing = Pairing.new(["Alice","Bob","Charly","Dan"])
     pairing.working_array
