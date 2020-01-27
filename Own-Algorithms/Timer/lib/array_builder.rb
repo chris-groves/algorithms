976c419c1ea53b98a508aeee514b47e982d732f3
Chris Groves <chris_groves87@hotmail.co.uk>
2020-01-27 17:52:31 +0000

    Array can now be increased in size by increments of 50000

--- Own-Algorithms/Timer/lib/array_builder.rb
+++ Own-Algorithms/Timer/lib/array_builder.rb
@@ -1,9 +1,19 @@
 class ArrayBuilder
   attr_accessor :array
 
+ARRAY_STARTING_LENGTH = 50000
+INCREASE_ARRAY_LENGTH_AMOUNT = 50000
+
   def initialize
-    @array = Array(1..50000)
+    @array_length = ARRAY_STARTING_LENGTH
+    @array = []
   end
 
+  def increase_array_size
+    @array_length += INCREASE_ARRAY_LENGTH_AMOUNT
+  end
 
+  def build_array
+    @array = Array(1..@array_length)
+  end
 end
--- Own-Algorithms/Timer/spec/array_builder_spec.rb
+++ Own-Algorithms/Timer/spec/array_builder_spec.rb
@@ -1,8 +1,16 @@
-require 'timer'
+require 'array_builder'
 
 describe ArrayBuilder do
   it 'builds an array of 50,000 in length' do
     array = ArrayBuilder.new
+    array.build_array
     expect(array.array.length).to eq 50000
+  end
+
+  it 'can increase in size by 50,000' do
+    array = ArrayBuilder.new
+    array.increase_array_size
+    array.build_array
+    expect(array.array.length).to eq 100000
   end
 end
